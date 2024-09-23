33e5fb4ec4b972189aa03c7cdd78d1271fe70686
Izhan Lara <izhanlaragarcia@gmail.com>
2024-09-23 11:54:48 +0200

    update

--- .history/vite-project/src/App_20240923115014.jsx
+++ .history/vite-project/src/App_20240923115014.jsx
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HeaderComponent from "./components/HeaderComponent";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Portfolio from "./components/subComponentes/Portfolio";
 import UpBtn from "./components/subComponentes/up-btn";
-
 function App() {
   const [language, setLanguage] = useState("en");
 
   const toggleLanguage = () => {
     setLanguage((prevLang) => (prevLang === "en" ? "es" : "en"));
   };
-
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HeaderComponent />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-      </Routes>
-      <UpBtn />
-    </Router>
+    <Routes>
+      <Route path="/" element={<HeaderComponent />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+    </Routes>
+    <UpBtn/>
+  </Router>
   );
 }
 
