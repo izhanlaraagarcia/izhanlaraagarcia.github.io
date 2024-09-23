ab44a3ac44539ccc80ed7c712a4bd8f85896aa54
Izhan Lara <izhanlaragarcia@gmail.com>
2024-09-23 12:16:17 +0200

    redirigir

--- .history/vite-project/vite.config_20240923121535.js
+++ .history/vite-project/vite.config_20240923121535.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/',  // Asegúrate de que este valor esté ajustado correctamente.
   assetsInclude: ['**/*.jpeg', '**/*.JPEG'],
 })
